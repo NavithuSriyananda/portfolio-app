5b2ec6325b3754704e699963803cd5dafd9152cd
EmpiteNavithu <navithusriyananda@gmail.com>
2023-10-10 00:53:16 +0530

    Fixed route issues

--- src/app/(default_site)/api/download-cv/route.tsx
+++ src/app/(default_site)/api/download-cv/route.tsx
@@ -1,18 +1,9 @@
 import 'server-only'
 
 import { NextRequest, NextResponse } from 'next/server';
-import { Readable } from 'stream'
-import { ReadFileAsync } from '@/libs/FileLibrary';
-import path from 'path';
-import { Blob, atob } from 'buffer';
-export async function GET(request: NextRequest) {
-    var file = await ReadFileAsync(path.join(process.cwd(), 'public/resume -Navithu Sriyananda.pdf'), 'base64');
-    const binaryString = atob(file.split(',')[1]); // Binary data string
 
-    var blob = new Blob([file], { type: 'pdf/*' });
-    const headers = new Headers();
 
-    headers.set("Content-Type", "pdf/*");
+export async function GET(request: Request) {
 
-    return new NextResponse(file, { status: 200, statusText: "OK", headers });
+    return Response.json({ "product": 123 })
 }
