24dfccff982d5f569fdf4394a820a71f3e04019e
EmpiteNavithu <navithusriyananda@gmail.com>
2023-10-11 23:04:01 +0530

    Navbar style changes

--- src/layouts/navbar/NavBar.tsx
+++ src/layouts/navbar/NavBar.tsx
@@ -44,58 +44,57 @@ export default function NavBar() {
 
 
             {/* left corner */}
-            <div></div>
+            <div className={`${Styles.left}`}>
+                <Link href={'/'}
+                    className={`${Styles.nav_logo} ${Styles.nav_item_hover}`}>
+                    <Image
+                        src={Logo}
+                        className={`${Styles.nav_logo_image}`}
+                        fill
+                        alt=''
+                    />
+                    {/* <h1 className="relative pr-4 pb-1 text-white after:transition-transform after:duration-500 after:ease-out after:absolute after:bottom-0 after:left-0 after:block after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-blue-500 after:content-[''] after:group-hover:origin-bottom-left after:group-hover:scale-x-100">Home</h1> */}
+                </Link>
+            </div>
 
             {/* center */}
-            <div></div>
+            <div className={`${Styles.center}`} >
+                <Link href={'/'}
+                    className={`${Styles.nav_item} ${Styles.nav_item_hover} ${(pathName == "/" && Styles.current)}`}>
+                    <FontAwesomeIcon
+                        icon={faHome}
+                        className='relative' />
+                    {/* <h1 className="relative pr-4 pb-1 text-white after:transition-transform after:duration-500 after:ease-out after:absolute after:bottom-0 after:left-0 after:block after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-blue-500 after:content-[''] after:group-hover:origin-bottom-left after:group-hover:scale-x-100">Home</h1> */}
+                    <h1 className="">Home</h1>
+                </Link>
+                <Link href={'/experience'}
+                    className={`${Styles.nav_item} ${Styles.nav_item_hover} ${(pathName == "/experience" && Styles.current)}`}>
+                    <FontAwesomeIcon
+                        icon={faRocket}
+                        className='relative' />
+                    <h1>Experience</h1>
+                </Link>
+                <Link href={'/projects'}
+                    className={`${Styles.nav_item} ${Styles.nav_item_hover} ${(pathName == "/projects" && Styles.current)}`}>
+                    <FontAwesomeIcon
+                        icon={faShapes}
+                        className='relative' />
+                    <h1>Projects</h1>
+                </Link >
+                <div
+                    className={`${Styles.nav_item}`}>
+                    <FontAwesomeIcon
+                        icon={faFilePdf}
+                        className='relative' />
+                    {/* <a href="https://drive.google.com/u/0/uc?id=1g6Yp06BiGdVKoUVy1zAwAYNv32FrW0f4&export=download" download="cv" onClick={onBtnDownloadClick}>Download CV</a> */}
+                    <a href='/public/resume -Navithu Sriyananda.pdf'>Donwload CV</a>
+                </div>
+            </div>
 
             {/* right corner */}
-            <div></div>
-
-            <Link href={'/'}
-                className={`${Styles.nav_logo} ${Styles.nav_item_hover}`}>
-                <Image
-                    src={Logo}
-                    className={`${Styles.nav_logo_image}`}
-                    fill
-                    alt=''
-                />
-                {/* <h1 className="relative pr-4 pb-1 text-white after:transition-transform after:duration-500 after:ease-out after:absolute after:bottom-0 after:left-0 after:block after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-blue-500 after:content-[''] after:group-hover:origin-bottom-left after:group-hover:scale-x-100">Home</h1> */}
-            </Link>
-
-            <Link href={'/'}
-                className={`${Styles.nav_item} ${Styles.nav_item_hover} ${(pathName == "/" && Styles.current)}`}>
-                <FontAwesomeIcon
-                    icon={faHome}
-                    className='relative' />
-                {/* <h1 className="relative pr-4 pb-1 text-white after:transition-transform after:duration-500 after:ease-out after:absolute after:bottom-0 after:left-0 after:block after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-blue-500 after:content-[''] after:group-hover:origin-bottom-left after:group-hover:scale-x-100">Home</h1> */}
-                <h1 className="">Home</h1>
-            </Link>
-
-            <Link href={'/experience'}
-                className={`${Styles.nav_item} ${Styles.nav_item_hover} ${(pathName == "/experience" && Styles.current)}`}>
-                <FontAwesomeIcon
-                    icon={faRocket}
-                    className='relative' />
-                <h1>Experience</h1>
-            </Link>
-
-            <Link href={'/projects'}
-                className={`${Styles.nav_item} ${Styles.nav_item_hover} ${(pathName == "/projects" && Styles.current)}`}>
-                <FontAwesomeIcon
-                    icon={faShapes}
-                    className='relative' />
-                <h1>Projects</h1>
-            </Link >
-
-            <div
-                className={`${Styles.nav_item}`}>
-                <FontAwesomeIcon
-                    icon={faFilePdf}
-                    className='relative' />
-                {/* <a href="https://drive.google.com/u/0/uc?id=1g6Yp06BiGdVKoUVy1zAwAYNv32FrW0f4&export=download" download="cv" onClick={onBtnDownloadClick}>Download CV</a> */}
-                <a href='/public/resume -Navithu Sriyananda.pdf'>Donwload CV</a>
+            <div className={`${Styles.right}`}>
             </div>
+
 
             {/* <div 
                 className={styles['nav-item']}>
