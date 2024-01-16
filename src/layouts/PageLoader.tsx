8b832ad5b849dc68520273808a33bec1317895ee
Navithu Sriyananda <navithusriyananda@gmail.com>
2024-01-16 22:58:13 +0530

    modified:   src/app/loading.tsx modified:   src/layouts/PageLoader.tsx

--- src/layouts/PageLoader.tsx
+++ src/layouts/PageLoader.tsx
@@ -9,7 +9,7 @@ export default function PageLoader() {
     const appState = UseAppContext();
     if (appState.IsLoading) {
         return (
-            <div className="flex fixed z-website-loader m-auto top-0 bottom-0 left-0 right-0 bg-white  justify-center items-center overflow-[show]">
+            <div className="flex fixed z-website-loader m-auto top-0 bottom-0 left-0 right-0 bg-white justify-center items-center overflow-[show]">
                 <FontAwesomeIcon icon={faGear} className="text-black text-[10vh] animate-[spin_2s_linear_infinite]" />
             </div>
         )
